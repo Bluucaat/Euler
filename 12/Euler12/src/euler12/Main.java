abb55cc7681597cb3649e5bbbd9fcdcd3d1b4412
Krisz <rybarkristik@gmail.com>
2023-08-31 17:45:29 +0200

    Euler 12 completed

---
+++ 12/Euler12/src/euler12/Main.java
@@ -0,0 +1,39 @@
+package euler12;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int divisorsFound = 0;
+		int i = 0;
+		boolean numberNotFound = true;
+
+		while (numberNotFound) {
+
+			divisorsFound = (int) getTotalDivisors(getTriangularNumber(i));
+			if (divisorsFound >= 500) {
+				System.out.println((long) getTriangularNumber(i) + " " + divisorsFound);
+				numberNotFound = false;
+			}
+			i++;
+		}
+	}
+
+	public static long getTotalDivisors(long n) {
+		int divisors = 0;
+		for (int i = 1; i*i < n; i++) {
+			if (n % i == 0) {
+				divisors += 2;
+				if (divisors >= 500) {
+					break;
+				}
+			}
+		}
+
+		return divisors;
+	}
+
+	public static long getTriangularNumber(int n) {
+		return (long)(n*(n+1))/2;
+
+	}
+}
