8f1f2c81697d2058bfe53b3cf213e73448b351df
Krisz <rybarkristik@gmail.com>
2023-09-01 20:22:10 +0200

    solved euler 15 (warning, code way too clean)

---
+++ 15/Euler15/src/Main.java
@@ -0,0 +1,1 @@
+public class Main { public static void main(String[] args) { long[][] grid = new long[21][21]; for (int i = 1; i < 21; i++) { for (int j = 1; j < 21; j++) { grid[i][j] += 1 + grid[i - 1][j] + grid[i][j - 1];} } System.out.println(grid[20][20] + 1);} }
